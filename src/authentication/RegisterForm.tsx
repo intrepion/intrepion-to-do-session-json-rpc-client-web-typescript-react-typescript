5fb0ac524950b454f5574c97b48001ca0df30eb2
Oliver Forral <intrepion@gmail.com>
2023-01-16 11:17:40 -0700

    green - add register api call

--- src/authentication/RegisterForm.tsx
+++ src/authentication/RegisterForm.tsx
@@ -1,3 +1,22 @@
-export default function RegisterForm() {
-  return <button>Register</button>;
+import axios from "axios";
+import { useState } from "react";
+
+export default function Register() {
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleRegister = async () => {
+    try {
+      const response = await axios.get("http://localhost:5166");
+      if (!response.data.error) {
+        setSuccessMessage("Successful registration!");
+      }
+    } catch (error) {}
+  };
+
+  return (
+    <>
+      <button onClick={handleRegister}>Register</button>
+      {successMessage && <p>{successMessage}</p>}
+    </>
+  );
 }
