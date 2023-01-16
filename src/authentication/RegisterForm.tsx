222898d0afe48ea2f90f0db9e4cf863b01e479e7
Oliver Forral <intrepion@gmail.com>
2023-01-16 11:17:50 -0700

    green - add more register fields

--- src/authentication/RegisterForm.tsx
+++ src/authentication/RegisterForm.tsx
@@ -4,19 +4,38 @@ import { useState } from "react";
 export default function Register() {
   const [successMessage, setSuccessMessage] = useState("");
 
-  const handleRegister = async () => {
+  const handleRegister = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
     try {
       const response = await axios.get("http://localhost:5166");
-      if (!response.data.error) {
+      if (response.data.result) {
         setSuccessMessage("Successful registration!");
       }
     } catch (error) {}
   };
 
   return (
-    <>
-      <button onClick={handleRegister}>Register</button>
+    <form onSubmit={handleRegister}>
+      <label htmlFor="username">
+        Username:
+        <input id="username" type="text" />
+      </label>
+      <label htmlFor="email">
+        Email:
+        <input id="email" type="email" />
+      </label>
+      <label htmlFor="password">
+        Password:
+        <input id="password" type="password" />
+      </label>
+      <label htmlFor="confirm">
+        Confirm:
+        <input id="confirm" type="password" />
+      </label>
+      <button id="register" type="submit">
+        Register
+      </button>
       {successMessage && <p>{successMessage}</p>}
-    </>
+    </form>
   );
 }
