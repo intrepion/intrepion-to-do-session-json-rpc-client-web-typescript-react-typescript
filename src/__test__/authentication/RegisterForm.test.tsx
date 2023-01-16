c20f43ac81246a1af6851021a59eaae989c3e796
Oliver Forral <intrepion@gmail.com>
2023-01-16 11:17:45 -0700

    red - add more register fields

--- src/__test__/authentication/RegisterForm.test.tsx
+++ src/__test__/authentication/RegisterForm.test.tsx
@@ -1,13 +1,30 @@
 import axios from "axios";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { v4 } from "uuid";
 import RegisterForm from "../../authentication/RegisterForm";
 
 describe("Registration Form", () => {
+  let confirmInput: HTMLElement;
+  let emailInput: HTMLElement;
   let registerButton: HTMLElement;
+  let passwordInput: HTMLElement;
+  let usernameInput: HTMLElement;
 
   beforeEach(() => {
     render(<RegisterForm />);
+    let confirmInputElement = screen.queryByLabelText("Confirm:");
+    if (!confirmInputElement) {
+      throw new Error("Confirm Input not found");
+    }
+    confirmInput = confirmInputElement;
+
+    let emailInputElement = screen.queryByLabelText("Email:");
+    if (!emailInputElement) {
+      throw new Error("Email Input not found");
+    }
+    emailInput = emailInputElement;
+
     let registerButtonElement = screen.queryByRole("button", {
       name: "Register",
     });
@@ -15,19 +32,39 @@ describe("Registration Form", () => {
       throw new Error("Register Button not found");
     }
     registerButton = registerButtonElement;
+
+    let passwordInputElement = screen.queryByLabelText("Password:");
+    if (!passwordInputElement) {
+      throw new Error("Password Input not found");
+    }
+    passwordInput = passwordInputElement;
+
+    let usernameInputElement = screen.queryByLabelText("Username:");
+    if (!usernameInputElement) {
+      throw new Error("Username Input not found");
+    }
+    usernameInput = usernameInputElement;
   });
 
-  it("has button to register", () => {
+  it("has form elements to register", () => {
     // Arrange
 
     // Act
 
     // Assert
+    expect(confirmInput).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
     expect(registerButton).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(usernameInput).toBeInTheDocument();
   });
 
   it("displays successful message", async () => {
     // Arrange
+    const username = v4();
+    const email = v4() + "@" + v4() + ".com";
+    const password = v4();
+
     const mockApiCall = jest.fn().mockResolvedValue({
       data: {
         id: "1",
@@ -39,9 +76,12 @@ describe("Registration Form", () => {
     axios.get = mockApiCall;
 
     // Act
+    userEvent.type(usernameInput, username);
+    userEvent.type(emailInput, email);
+    userEvent.type(passwordInput, password);
+    userEvent.type(confirmInput, password);
     userEvent.click(registerButton);
 
-    // Assert
     const message = await screen.findByText("Successful registration!");
     expect(message).toBeInTheDocument();
   });
